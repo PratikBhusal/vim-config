f8287f79d1ea7614076cfc338b842590c180f4b8
PratikBhusal <PratikBhusal@users.noreply.github.com>
2018-12-11 15:48:48 -0600

    Added method to install dotfiles

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function dotfiles {
+    git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+if [[ -x "$(command -v git)" && -x "$(command -v rsync)" &&  ! -d $HOME/.dotfiles ]]; then
+    git clone --separate-git-dir=$HOME/.dotfiles git@github.com:PratikBhusal/dotfiles.git tmpdotfiles
+
+    rsync --recursive --verbose --links --exclude '.git' tmpdotfiles/ $HOME/
+    rm -r tmpdotfiles
+
+    dotfiles config --local status.showUntrackedFiles no
+elif [[ ! -x "$(command -v git)" || ! -x "$(command -v rsync)" ]]; then
+    echo "please install git and rsync before trying to install dotfiles"
+fi
